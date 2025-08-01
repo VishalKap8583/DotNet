db3d05aca96b3344b5da18c96eed83d113d0828b
VishalKap8583 <vishalkap05.vk@gmail.com>
2025-08-01 16:24:33 +0530

    Basics

---
+++ basic1/Program.cs
@@ -0,0 +1,33 @@
+﻿// See https://aka.ms/new-console-template for more information
+int a = 1;
+bool status = true;
+string message = "abc";
+
+Console.WriteLine($"a: {a}, status: {status}, Message: {message}");
+
+if (status)
+{
+    Console.WriteLine("Status is true");
+}
+else
+{
+    Console.WriteLine("Status is false");
+}
+
+Console.WriteLine("Choose an option:");
+Console.WriteLine("1. Option 1");
+Console.WriteLine("2. Option 2");
+
+int choice = Convert.ToInt32(Console.ReadLine());
+switch (choice)
+{
+    case 1:
+        Console.WriteLine("You chose option 1");
+        break;
+    case 2:
+        Console.WriteLine("You chose option 2");
+        break;
+    default:
+        Console.WriteLine("Invalid choice");
+        break;
+}
---
+++ basic2/Program.cs
@@ -0,0 +1,25 @@
+﻿int a1, a2, a3;
+
+for (int i = 1; i <= 3; i++)
+{
+    Console.WriteLine($"Enter value for a{i}:");
+    if (!int.TryParse(Console.ReadLine(), out int value))
+    {
+        Console.WriteLine("Invalid input, please enter an integer.");
+        i--; // Decrement to repeat the input for the same variable
+        continue;
+    }
+    
+    switch (i)
+    {
+        case 1:
+            a1 = value;
+            break;
+        case 2:
+            a2 = value;
+            break;
+        case 3:
+            a3 = value;
+            break;
+    }
+}
