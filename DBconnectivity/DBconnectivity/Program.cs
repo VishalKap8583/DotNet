8b6c3816733ae9b1de9d7099112e1b349660820b
VishalKap8583 <vishalkap05.vk@gmail.com>
2025-08-04 11:08:58 +0530

    DBconnectivity Samples

---
+++ DBconnectivity/DBconnectivity/Program.cs
@@ -0,0 +1,26 @@
+﻿using MySql.Data.MySqlClient;
+using System.Data;
+
+String ConnectionString ="Server=localhost;Port=3306; Database=employeeslist;User ID=root;Password=password;";
+
+MySqlConnection connection = new MySqlConnection(ConnectionString);
+    try
+    {
+        connection.Open();
+        MySqlCommand cmd = new MySqlCommand("SELECT * FROM employees", connection);
+        MySqlDataReader reader = cmd.ExecuteReader();
+        while (reader.Read())
+        {
+            Console.WriteLine($"ID: {reader["ID"]}, Name: {reader["Name"]}, Position: {reader["Position"]}");
+        }
+    }
+    catch (MySqlException ex)
+    {
+        Console.WriteLine($"Error: {ex.Message}");
+    }
+    finally
+    {
+        connection.Close();
+    }
+
+
